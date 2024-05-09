628dcdccef458f817bff7a5ee0b03e97b7c6a963
MuhammadFasya <@mhmdfasya15@gmail.com>
2024-05-09 15:42:21 +0700

    progress

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 // Ini JavaScript
+
+function validation() {
+  console.log("Good");
+}
+
+// Section Slide
+let indexSlide = 1; // [0, 1, 2]
+showSlide(1);
+
+// Trigger Slide
+function nextSlide(n) {
+  console.log("nextSlide: " + n);
+  showSlide((indexSlide += n));
+}
+
+// Slide Logic
+function showSlide(index) {
+  let listImage = document.getElementsByClassName("banner-item");
+  console.log(listImage);
+  console.log("Banyaknya image: " + listImage.length);
+
+  //  Reset Slide
+  if (index > listImage.length) indexSlide = 1;
+
+  // Hide all images
+  let i = 0;
+  while (i < listImage.length) {
+    listImage[i].style.display = "none";
+    i++;
+  }
+
+  // Show only one image
+  listImage[indexSlide - 1].style.display = "block";
+  console.log("showSlide: " + index);
+}
+
+setInterval(() => nextSlide(1), 2000);
+
+document
+  .getElementById("home-btn")
+  .addEventListener("click", () => nextSlide(1));
